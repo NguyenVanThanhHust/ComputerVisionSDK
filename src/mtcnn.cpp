6397e8e41389f554651c27da24bcf04df0a1830f
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2020-10-26 16:56:44 +0700

    nms + refine

--- src/mtcnn.cpp
+++ src/mtcnn.cpp
@@ -154,3 +154,97 @@ void MTCNN::nmsTwoBoxs(vector<Bbox>& boundingBox_, vector<Bbox>& previousBox_, c
 	}
 	//std::cout << boundingBox_.size() << std::endl;
 }
+
+void MTCNN::nms(std::vector<Bbox> &boundingBox_, const float overlap_threshold, string modelname){
+    if(boundingBox_.empty()){
+        return;
+    }
+    sort(boundingBox_.begin(), boundingBox_.end(), cmpScore);
+    float IOU = 0;
+    float maxX = 0;
+    float maxY = 0;
+    float minX = 0;
+    float minY = 0;
+    std::vector<int> vPick;
+    int nPick = 0;
+    std::multimap<float, int> vScores;
+    const int num_boxes = boundingBox_.size();
+	vPick.resize(num_boxes);
+	for (int i = 0; i < num_boxes; ++i){
+		vScores.insert(std::pair<float, int>(boundingBox_[i].score, i));
+	}
+    while(vScores.size() > 0){
+        int last = vScores.rbegin()->second;
+        vPick[nPick] = last;
+        nPick += 1;
+        for (std::multimap<float, int>::iterator it = vScores.begin(); it != vScores.end();){
+            int it_idx = it->second;
+            maxX = std::max(boundingBox_.at(it_idx).x1, boundingBox_.at(last).x1);
+            maxY = std::max(boundingBox_.at(it_idx).y1, boundingBox_.at(last).y1);
+            minX = std::min(boundingBox_.at(it_idx).x2, boundingBox_.at(last).x2);
+            minY = std::min(boundingBox_.at(it_idx).y2, boundingBox_.at(last).y2);
+            //maxX1 and maxY1 reuse 
+            maxX = ((minX-maxX+1)>0)? (minX-maxX+1) : 0;
+            maxY = ((minY-maxY+1)>0)? (minY-maxY+1) : 0;
+            //IOU reuse for the area of two bbox
+            IOU = maxX * maxY;
+            if(!modelname.compare("Union"))
+                IOU = IOU/(boundingBox_.at(it_idx).area + boundingBox_.at(last).area - IOU);
+            else if(!modelname.compare("Min")){
+                IOU = IOU/((boundingBox_.at(it_idx).area < boundingBox_.at(last).area)? boundingBox_.at(it_idx).area : boundingBox_.at(last).area);
+            }
+            if(IOU > overlap_threshold){
+                it = vScores.erase(it);
+            }else{
+                it++;
+            }
+        }
+    }
+    
+    vPick.resize(nPick);
+    std::vector<Bbox> tmp_;
+    tmp_.resize(nPick);
+    for(int i = 0; i < nPick; i++){
+        tmp_[i] = boundingBox_[vPick[i]];
+    }
+    boundingBox_ = tmp_;
+}
+void MTCNN::refine(vector<Bbox> &vecBbox, const int &height, const int &width, bool square){
+    if(vecBbox.empty()){
+        cout<<"Bbox is empty!!"<<endl;
+        return;
+    }
+    float bbw=0, bbh=0, maxSide=0;
+    float h = 0, w = 0;
+    float x1=0, y1=0, x2=0, y2=0;
+    for(vector<Bbox>::iterator it=vecBbox.begin(); it!=vecBbox.end();it++){
+        bbw = (*it).x2 - (*it).x1 + 1;
+        bbh = (*it).y2 - (*it).y1 + 1;
+        x1 = (*it).x1 + (*it).regreCoord[0]*bbw;
+        y1 = (*it).y1 + (*it).regreCoord[1]*bbh;
+        x2 = (*it).x2 + (*it).regreCoord[2]*bbw;
+        y2 = (*it).y2 + (*it).regreCoord[3]*bbh;
+
+        
+        
+        if(square){
+            w = x2 - x1 + 1;
+            h = y2 - y1 + 1;
+            maxSide = (h>w)?h:w;
+            x1 = x1 + w*0.5 - maxSide*0.5;
+            y1 = y1 + h*0.5 - maxSide*0.5;
+            (*it).x2 = round(x1 + maxSide - 1);
+            (*it).y2 = round(y1 + maxSide - 1);
+            (*it).x1 = round(x1);
+            (*it).y1 = round(y1);
+        }
+
+        //boundary check
+        if((*it).x1<0)(*it).x1=0;
+        if((*it).y1<0)(*it).y1=0;
+        if((*it).x2>width)(*it).x2 = width - 1;
+        if((*it).y2>height)(*it).y2 = height - 1;
+
+        it->area = (it->x2 - it->x1)*(it->y2 - it->y1);
+    }
+}
