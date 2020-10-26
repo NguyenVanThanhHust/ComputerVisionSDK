f07993594139fd8d0084172f4c62adeecd128e20
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2020-10-26 16:09:18 +0700

    detect in sdk

--- src/sdk.cpp
+++ src/sdk.cpp
@@ -41,6 +41,34 @@ ErrorCode SDK::Impl::getFaceBoxAndLandmarks(const std::string &imgPath, bool &fa
 
     m_mtcnn.detect(ncnnImg, bboxVec);
     
+    if (bboxVec.empty()) {
+        faceDetected = false;
+        return ErrorCode::NO_ERROR;
+    }
+
+    faceDetected = true;
+    fbAndLandmarksVec.clear();
+    fbAndLandmarksVec.reserve(bboxVec.size());
+
+    for (const auto& bbox: bboxVec) {
+        FaceBoxAndLandmarks fb;
+        fb.topLeft.x = bbox.x1;
+        fb.topLeft.y = bbox.y1;
+        fb.bottomRight.x = bbox.x2;
+        fb.bottomRight.y = bbox.y2;
+
+        for (int i = 0; i < 5; ++i) {
+            Point p;
+            p.x = static_cast<int>(bbox.landmark.x[i]);
+            p.y = static_cast<int>(bbox.landmark.y[i]);
+
+            fb.landmarks[i] = p;
+        }
+
+        fbAndLandmarksVec.emplace_back(fb);
+    }
+
+    return ErrorCode::NO_ERROR;
 
 }
 
